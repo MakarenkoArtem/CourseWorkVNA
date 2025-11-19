6b1955d274f0ba7f3eaa0d9f1f49d65210158ad9
Artem <makarenkoiandeks@yandex.ru>
2025-11-19 21:48:26 +0300

    writing second driver version

---
+++ driver/build/CMakeFiles/vna_example.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vna_example.dir/VNAKitDevice.cpp.o"
+  "CMakeFiles/vna_example.dir/VNAKitDevice.cpp.o.d"
+  "CMakeFiles/vna_example.dir/main.cpp.o"
+  "CMakeFiles/vna_example.dir/main.cpp.o.d"
+  "vna_example"
+  "vna_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vna_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ driver/build/CMakeFiles/vna_example.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for vna_example.
+# This may be replaced when dependencies are built.
