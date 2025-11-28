abf82d23e1634b6ce5f4b6b7628db93f93bf45d8
Artem <makarenkoiandeks@yandex.ru>
2025-11-28 20:46:48 +0300

    added login

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
+import os
+
 from flask import Flask, request, jsonify, send_from_directory, render_template, redirect
 from flask_cors import CORS
 from flask_login import LoginManager, current_user, login_user, login_required, logout_user
-from forms.user import RegisterForm
+from werkzeug.security import check_password_hash
+
+from forms.user import RegisterForm, EntryForm
 from data import db_session
 from data.users import User
 import datetime
@@ -11,7 +15,7 @@ app = Flask(
     # template_folder="react-app",  # Jinja2 HTML
     # static_folder=None  # Статику подключаем вручную ниже
 )
-app.config["SECRET_KEY"] = "your-secret-key"  # нужен для CSRF и сессий
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "key")  # нужен для CSRF и сессий
 app.config["PERMANENT_SESSION_LIFETIME"] = datetime.timedelta(days=1)
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -34,7 +38,7 @@ def send_lib(path):
 
 # ------------------------  TEMPLATES  ---------------------------
 
-@app.route("/")
+@app.route("/main")
 def home():
     name = ''
     try:
@@ -119,6 +123,26 @@ def register():  # форма для регистрации
         return redirect('/')
     return render_template('register.html', title='Регистрация', form=form, id=0)
 
+@app.route('/')
+def choice():  # выбор входа или регистрации
+    return render_template("choice.html")
+
+
+@app.route('/entry', methods=['GET', 'POST'])
+def entry():  # форма для входа
+    form = EntryForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        curUser = None
+        for user in db_sess.query(User).all():
+            if user.email == form.email.data and check_password_hash(user.hashed_password, form.password.data):
+                curUser = user
+                break
+        if curUser is None:
+            return render_template('entry.html', form=form, message="Такой пользователь не найден")
+        login_user(curUser, remember=True)
+        return redirect('/main')
+    return render_template('entry.html', form=form)
 
 @app.route('/logout')
 @login_required
